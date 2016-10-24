10624ab6a04f05a90104dc1347c787d994e0e6ba
Benjamin Tennant <benjamin.tennant07@gmail.com>
2016-10-24 20:42:43 +0800

    Added levelBuilder.c which is an application for designing levels for game

---
+++ levelBuilder.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+//This block will be moved to a common file////////
+
+	typedef enum
+	{
+		desert,
+		woods,
+		lake,
+		mountains,
+		grasslands,
+		village,
+		path,
+		number_of_places
+	} places;
+
+	int max_level_size = 20;
+
+///////////////////////////////////////////////////
+
+	FILE *outputFile = fopen("level.bin", "w");
+	int level_size = 0;
+
+	while(level_size == 0)
+	{
+		printf("how big do you want your level?: ");
+		scanf("%d", &level_size);
+
+		if(level_size <= 0 || level_size > max_level_size)
+		{
+			printf("Your level must be a number between 1 and %d\n", max_level_size);
+			level_size = 0;
+		}
+	}
+	fwrite(&level_size, sizeof(places), 1, outputFile);
+
+	int level[level_size];
+
+	for(int index = 0; index < level_size; ++index)
+	{
+		printf("Enter the type of place for position %d \n", index);
+		printf("desert = 0 \nwoods = 1 \nlake = 2 \nmountains = 3 \ngrasslands = 4 \nvillage = 5 \npath = 6 \n: ");
+
+		scanf("%d", &level[index]);
+
+		if(level[index] < 0 || level[index] >= number_of_places)
+		{
+			printf("The type of place must be a number between 0 and %d\n", number_of_places - 1);
+			--index;
+		}
+	}
+
+	printf("Your level looks like this:\n");
+
+	for(int index = 0; index < level_size; ++index)
+	{
+		switch(level[index])
+		{
+			case desert:
+				printf("desert\n");
+				break;
+			case woods:
+				printf("woods\n");
+				break;
+			case lake:
+                                printf("lake\n");
+                                break;
+			case mountains:
+                                printf("mountains\n");
+                                break;
+
+			case grasslands:
+                                printf("grasslands\n");
+                                break;
+
+                        case village:
+                                printf("village\n");
+                                break;
+
+                        case path:
+                                printf("path\n");
+                                break;
+
+
+			default:
+				printf("%d\n", level[index]);
+				break;
+		}
+	}
+		
+	fwrite(level, sizeof(places), level_size, outputFile);
+	
+	fclose(outputFile);
+	
+	return 0;
+}
+
